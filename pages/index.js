6ea63ff73aa519093ce1ed2d72fef4deb5e54259
Fabio Gomes de Abreu <noisabreus@hotmail.com>
2024-11-20 21:28:59 +0000

    Mensagem index atualizada

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,12 @@ function Home() {
   return (
     <>
       <h1>Bem Vindo comunidade dos desenvolvedores indies do Brasil</h1>
+      Estamos neste exato momento construindo uma nova pagina na internet para
+      quem trabalha com desenvolvimento de jogos e precisa de espaço para
+      crescer em comuinidade.
+      <br />
+      Somos sonhadora e dedicadas, inclusive estamos neste momento colocando a
+      mão na massa
       <br />
       Pagina em construção
     </>

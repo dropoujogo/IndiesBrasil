555c6c2a19fc3d3defa611b0a19bf7e0511d0f02
Fabio Gomes de Abreu <noisabreus@hotmail.com>
2024-11-20 01:52:10 +0000

    update message

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home(){
-    return <><h1>Comunidade Indies Brasil</h1><br/>Pagina em construção</>
+    return <><h1>Bem Vindo comunidade dos desenvolvedores indies do Brasil</h1><br/>Pagina em construção</>
 }
 
 export default Home;

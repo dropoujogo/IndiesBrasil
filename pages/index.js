d62598358a7dfc50f1d33952875674ba4ab887f7
Fabio Gomes de Abreu <noisabreus@hotmail.com>
2024-11-20 18:19:58 +0000

    adicionado `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,11 @@
-function Home(){
-  return <><h1>Bem Vindo comunidade dos desenvolvedores indies do Brasil</h1><br/>Pagina em construção</>
+function Home() {
+  return (
+    <>
+      <h1>Bem Vindo comunidade dos desenvolvedores indies do Brasil</h1>
+      <br />
+      Pagina em construção
+    </>
+  );
 }
 
 export default Home;

b0fbfeb32940d2acf406faf1626828b307f19295
Fabio Gomes de Abreu <noisabreus@hotmail.com>
2024-11-26 00:45:25 +0000

    Ata primeira reunião

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,43 @@ function Home() {
       <br />
       Pagina em construção
       <br />
-      Hoje participaram da primeira assembleia: Fabio, Patric, Gustavo e Camilla
+      <code>
+        Hoje participaram da primeira assembleia: Fabio, Patrick, Gustavo e
+        Camilla
+        <br />
+        Os temas da primeira assembleia foram:
+        <br />
+        -Periodicidade de reuniões:
+        <br />
+        Ficou decidido que iremos fazer uma enquete para saber a disponibilidade
+        dos membros.
+        <br />
+        Fazer a proxima reunião na proxima semana para ver a participação.
+        <br />
+        Tarefas da semana:
+        <br />
+        Artes dos canais (Youtube, Discord e Whattap) quem pode se volutariar
+        <br />
+        Definir estratégias nas redes sociais:
+        <br />
+        Por enquanto manter as que temos para no futuro se arriscar no instagram
+        e bluesky
+        <br />
+        Aprender como engajar nas redes sociais.
+        <br />
+        Discussão sobre o que o que queremos ser:
+        <br />
+        Fiquei responsável de disponibilizar um material sobre cada tipo de
+        organização(Sindicato,Associação, Coletivo).
+        <br />
+        EM seguida quando decidirmos na proxima reunião o que seremos, discutir
+        sobre a união com o UNIDADE ou outras associações
+        <br />
+        Membros se cadastrar na Abragames e Sebrae
+        <br />
+        Levantamento dos membros para criação do material de divulgação e em
+        seguida conversar com parceiros.
+      </code>
     </>
   );
 }

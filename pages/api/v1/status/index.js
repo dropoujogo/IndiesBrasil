37648e4354361c7102f24d7a6828bfba54b097c3
Fabio Gomes de Abreu <noisabreus@hotmail.com>
2024-12-05 11:35:35 +0000

    query mais robusta

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,30 @@
 import database from "infra/database.js";
 
 async function status(request, response) {
-  const result = await database.query("Select 1+1 as sum;");
-  console.log(result.rows);
-  response.status(200).json({ chave: "Status OK" });
+  const updatedAt = new Date().toISOString();
+  const databaseVersionResult = await database.query("SHOW server_version;");
+  const databaseVersionValue = databaseVersionResult.rows[0].server_version;
+
+  const databaseMaxConnResult = await database.query("SHOW max_connections;");
+  const databaseMaxConnValue = databaseMaxConnResult.rows[0].max_connections;
+
+  const databaseName = process.env.POSTGRES_DB;
+  const databaseOpenConnResult = await database.query({
+    text: "Select count(*)::int from pg_stat_activity where datname=$1;",
+    values: [databaseName],
+  });
+  const databaseOpenConnValue = databaseOpenConnResult.rows[0].count;
+
+  response.status(200).json({
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version: databaseVersionValue,
+        max_connections: parseInt(databaseMaxConnValue),
+        opened_connections: databaseOpenConnValue,
+      },
+    },
+  });
 }
 
 export default status;

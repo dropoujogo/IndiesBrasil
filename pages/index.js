0c605d7b1495896e90eb95987b4aa63d9671fc18
Fabio Gomes de Abreu <noisabreus@hotmail.com>
2025-02-12 01:10:28 +0000

    feat: add initial version of `/status` page

--- pages/index.js
+++ pages/index.js
@@ -11,47 +11,7 @@ function Home() {
       <br />
       Pagina em construção
       <br />
-      <code>
-        Hoje participaram da primeira assembleia: Fabio, Patrick, Gustavo e
-        Camilla
-        <br />
-        -- Os temas da primeira assembleia foram:
-        <br />
-        ---- Periodicidade de reuniões:
-        <br />
-        Ficou decidido que iremos fazer uma enquete para saber a disponibilidade
-        dos membros.
-        <br />
-        Fazer a proxima reunião na proxima semana para ver a participação.
-        <br />
-        ---- Tarefas da semana:
-        <br />
-        Artes dos canais (Youtube, Discord e Whattap) quem pode se volutariar
-        <br />
-        ---- Definir estratégias nas redes sociais:
-        <br />
-        Usar a organização como chamariz de curiosidade para atrair seguidores e
-        novos membros. Por enquanto manter as que temos para no futuro se
-        arriscar no instagram e bluesky
-        <br />
-        Aprender como engajar nas redes sociais.
-        <br />
-        ---- Discussão sobre o que o que queremos ser:
-        <br />
-        Fiquei responsável de disponibilizar um material sobre cada tipo de
-        organização(Sindicato,Associação, Coletivo).
-        <br />
-        EM seguida quando decidirmos na proxima reunião o que seremos, discutir
-        sobre a união com o UNIDADE ou outras associações
-        <br />
-        Membros se cadastrar na Abragames e Sebrae
-        <br />
-        Levantamento dos membros para criação do material de divulgação e em
-        seguida conversar com parceiros.
-        <br />
-        ---- planos para o futuro: nosso proprio site, distribuidora e sistema
-        de vendas
-      </code>
+      <code>- meta atual: criação de usuário e autenticação</code>
     </>
   );
 }
